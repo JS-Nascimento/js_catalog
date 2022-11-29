440c90f412151f9c65411ab57cb9575c20152a37
JS-Nascimento <jorge@jstec.dev.br>
2022-11-29 00:49:19 -0300

    theme customization

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import 'react-bootstrap/dist/react-bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
+
 import App from './App';
 
 
